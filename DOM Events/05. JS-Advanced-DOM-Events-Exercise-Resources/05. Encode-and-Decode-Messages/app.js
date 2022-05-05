3abbc5a90bdcb660ae82761185e8c764b5df005a
krast0 <devilmarti@abv.bg>
2022-05-05 08:27:51 +0300

    Exercises

--- DOM Events/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ DOM Events/05. JS-Advanced-DOM-Events-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    
+    let EncodeTextAreaElement = document.querySelector('#main textarea')
+    let EncodeButtonElement = document.querySelector('#main button')
+    let DecodeTextAreaElement = document.querySelectorAll('#main textarea')[1]
+    let DecodeButtonElement = document.querySelectorAll('#main button')[1]
+    let decoded = ''
+
+    EncodeButtonElement.addEventListener('click',Encode)
+
+    DecodeButtonElement.addEventListener('click',Decode)
+
+    function Decode(e){
+        DecodeTextAreaElement.value = decoded
+    }
+
+    function Encode(e){
+        let textToDecode = EncodeTextAreaElement.value
+        if(!textToDecode.length == 0){
+        decoded = textToDecode
+        let array = []
+        let string = ''
+        for (let el of textToDecode) {
+            let convertedEl = el.charCodeAt() + 1
+            array.push(convertedEl)
+        }
+
+        for (let el of array) {
+            let convertedASCII = String.fromCharCode(el)
+            string += convertedASCII
+        }
+        EncodeTextAreaElement.value = ''
+        DecodeTextAreaElement.value = string
+    }else{
+        return null
+    }
+    }
+    
 }
--- DOM Events/05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
+++ DOM Events/05. JS-Advanced-DOM-Events-Exercise-Resources/06. Furniture/solution.js
@@ -1,4 +1,70 @@
 function solve() {
+  let textAreaElement = document.querySelectorAll("#exercise textarea")[0];
+  let GenareteButtonElement = document.querySelectorAll("#exercise button")[0];
+  let bodyOfTable = document.querySelector("tbody");
+  let buyButtonElement = document.querySelectorAll("#exercise button")[1];
+  let textOutputAreaElement =
+    document.querySelectorAll("#exercise textarea")[1];
 
-  //TODO...
+  GenareteButtonElement.addEventListener("click", AddFurniture);
+
+  function AddFurniture(e) {
+    let input = JSON.parse(textAreaElement.value);
+    for (let el of input) {
+      let tableRowElement = document.createElement("tr");
+
+      let furnitureName = document.createElement("td");
+      furnitureName.textContent = el.name;
+      furnitureName.id = "name";
+      let image = document.createElement("td");
+      let img = document.createElement("img");
+      img.src = el.img;
+      image.appendChild(img);
+      image.id = "image";
+      let furnitureprice = document.createElement("td");
+      furnitureprice.textContent = el.price;
+      furnitureprice.id = "price";
+      let furnitureDec = document.createElement("td");
+      furnitureDec.textContent = el.decFactor;
+      furnitureDec.id = "dec";
+      let checkBoxElement = document.createElement("td");
+      let checkMark = document.createElement("input");
+      checkMark.type = "checkbox";
+      checkBoxElement.appendChild(checkMark);
+
+      tableRowElement.appendChild(image);
+      tableRowElement.appendChild(furnitureName);
+      tableRowElement.appendChild(furnitureprice);
+      tableRowElement.appendChild(furnitureDec);
+      tableRowElement.appendChild(checkBoxElement);
+
+      bodyOfTable.appendChild(tableRowElement);
+    }
+  }
+  buyButtonElement.addEventListener("click", Buy);
+  function Buy(e) {
+    let sum = 0;
+    let checkBoxElements = document.querySelectorAll("input[type = 'checkbox']");
+    console.log(checkBoxElements);
+    let arrayOfNames = [];
+    let sumOfDecFactor = 0;
+
+    let count = 0;
+    for (let element of checkBoxElements) {
+      let isChecked = element.checked;
+      if (isChecked) {
+        count++;
+        let price = element.parentElement.parentElement.querySelector("#price");
+        let name = element.parentElement.parentElement.querySelector("#name");
+        let dec = element.parentElement.parentElement.querySelector("#dec");
+        arrayOfNames.push(name.textContent);
+        sum += Number(price.textContent);
+        sumOfDecFactor += Number(dec.textContent);
+      }
+    }
+    let avarageDecFactor = sumOfDecFactor / count;
+    textOutputAreaElement.textContent = `Bought furniture: ${arrayOfNames.join(
+      ", "
+    )}\nTotal price: ${sum.toFixed(2)}\nAverage decoration factor: ${avarageDecFactor}`;
+  }
 }
